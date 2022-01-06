f89ba3e129a95c99e70b4877e8e8079153bf83da
xonfps <isaque.souza@lunes.io>
2022-01-06 18:29:39 +0000

    add: a guessing game

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+
+    let secret_number = rand::thread_rng().gen_range(1..11);
+    
+    println!("Guess the number!");
+
+    loop {
+        println!("Please input your guess: ");
+        
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("To Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+                }
+            }
+        }
+}
